b20323f99fde51b5917678fc6f4187793c6a4958
Abe Calder <abecalder1@gmail.com>
2025-11-04 18:48:59 +1300

    complete for loop to iterate through taxbrackets and calculate tax to add to taxDeduction variable

--- taxCalculator.ts
+++ taxCalculator.ts
@@ -4,21 +4,39 @@
 // $78,101 - $180,000	33%
 // $180,001 and over	39%
 
-
+// taxBrackets array with objects of threshold and applicable tax rate
 const taxBrackets = [
-  {threshold: 180000, rate: 0.39},
-  {threshold: 78100, rate: 0.33},
-  {threshold: 53500, rate: 0.30},
-  {threshold: 15600, rate: 0.175},
-  {threshold: 0, rate: 0.105}
+  { threshold: 180000, rate: 0.39 },
+  { threshold: 78100, rate: 0.33 },
+  { threshold: 53500, rate: 0.30 },
+  { threshold: 15600, rate: 0.175 },
+  { threshold: 0, rate: 0.105 }
 ]
 
 function getIncomeTaxRate(inputAmount: number): number {
   let taxDeductions = 0
 
-  const incomeToTax = inputAmount
+  // totalAmount constant for the if statment in the loop
+  const totalAmount = inputAmount
 
+  // incomeToTax variable that will change within the loop
+  let incomeToTax = totalAmount
 
+  for (let i = 0 ; i < taxBrackets.length ; i++ ) {
+    // define individual brackets
+    const bracket = taxBrackets[i]
+    // check if totalAmount is more than bracket.threshold
+    if (totalAmount > bracket.threshold) {
+      // reduce bracket threshold from incomeToTax
+      const amountInBracket = incomeToTax - bracket.threshold
+      // add the tax calc for the bracket to taxDeductions
+      taxDeductions += amountInBracket * bracket.rate
+      // reasign incomeToTax to be the bracket threshold
+      incomeToTax = bracket.threshold
+    }
+  }
+
+  
 
   return
 }
